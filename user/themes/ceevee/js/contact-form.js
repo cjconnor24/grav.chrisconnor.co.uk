516fd5d885749d2f6b080acb9778871a95573591
Chris Connor <chris@chrisconnor.co.uk>
2018-10-11 11:56:21 +0100

    Adding AJAX code

---
+++ user/themes/ceevee/js/contact-form.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    console.log('doc is ready');
+
+    var form = $('#contact-form');
+    form.submit(function(e) {
+        // prevent form submission
+        e.preventDefault();
+
+        console.log('SUBMITTED');
+
+        // submit the form via Ajax
+        $.ajax({
+            url: form.attr('action'),
+            type: form.attr('method'),
+            dataType: 'html',
+            data: form.serialize(),
+            success: function(result) {
+                // Inject the result in the HTML
+                $('#form-result').html(result);
+            }
+        });
+    });
+});
